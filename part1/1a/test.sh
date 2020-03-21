288d51ed3eea97cb8fdf55857909e50ac6d5c5dd
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-03-21 17:17:57 -0400

    Update test.sh

--- part1/1a/test.sh
+++ part1/1a/test.sh
@@ -8,8 +8,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /1a/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../parking-violations-data/Parking_Violations.csv* /1a/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file /project1/part1/1a/mapper.py -mapper /project1/part1/1a/mapper.py \
--file /project1/part1/1a/reducer.py -reducer /project1/part1/1a/reducer.py \
+-file ../../mapreduce-project1/part1/1a/mapper.py -mapper ../../mapreduce-project1/part1/1a/mapper.py \
+-file ../../mapreduce-project1/part1/1a/reducer.py -reducer ../../mapreduce-project1/part1/1a/reducer.py \
 -input /1a/input/* -output /1a/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /1a/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /1a/input/
