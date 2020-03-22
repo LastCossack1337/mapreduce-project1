a0dd787d34992178af4a0df9a42a17d993c027ea
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-03-21 20:24:49 -0400

    Update reducer.py

--- part1/1a/reducer.py
+++ part1/1a/reducer.py
@@ -6,15 +6,14 @@ dict_month_count = {}
 
 for line in sys.stdin:
     line = line.strip()
-    month, num = line.split('\t')
+    color, num = line.split('\t')
     try:
         num = int(num)
-        dict_month_count[month] = dict_month_count.get(month, 0) + num
-
+        dict_color_count[color] = dict_color_count.get(color, 0) + num
+    
     except ValueError:
         pass
-
-
-sorted_dict_month_count = sorted(dict_month_count.items(), key=itemgetter(1), reverse = True)
-for month, count in sorted_dict_month_count:
-    print '%s\t%s' % (month, count)
+    
+sorted_dict_color_count = sorted(dict_color_count.items(), key=itemgetter(1), reverse = True)
+for color, count in sorted_dict_month_count:
+    print '%s\t%s' % (color, count)
