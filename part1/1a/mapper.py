7d0ac250014ff98cfeda12218b53b29b4d8f7716
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-03-22 17:16:52 -0400

    Update mapper.py

--- part1/1a/mapper.py
+++ part1/1a/mapper.py
@@ -3,9 +3,10 @@
 from operator import itemgetter
 import sys
 
-for line in sys.stdin:
-    vehiclecolor = line.split(",")[33].strip()
-
     if vehiclecolor:
         issuecolor = str(vehiclecolor)
+        issuecolor == "Vehicle Color"
+        continue
+    else:
+
         print("%s\t%s" % (issuecolor, 1))
