24601c08213df8996ef0a0c42e2e13333d97b748
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-03-21 21:48:46 -0400

    Update mapper.py

--- part1/1a/mapper.py
+++ part1/1a/mapper.py
@@ -4,7 +4,7 @@ import sys
 
 for line in sys.stdin:
     line = line.split(",")
-    vehiclecolor = line[33]
+    vehiclecolor = line[35]
     try:
         issuecolor = str(vehiclecolor)
         print (("%s\t%s") % (issuecolor, 1))
