798ed364b4d3502b79e0d22f07f29e5e088ec301
LastCossack1337 <57050312+LastCossack1337@users.noreply.github.com>
2020-03-22 19:14:26 -0400

    Update mapper.py

--- part1/1a/mapper.py
+++ part1/1a/mapper.py
@@ -3,10 +3,21 @@
 from operator import itemgetter
 import sys
 
-for line in sys.stdin:
+# Dictionary with all the colors that you have identified so far
+color_dict = {
+    "BLACK":["BLK","BLACK","BLA","BK"],
+    "WHITE":["WHITE","WHT","WHIT","WH","WT"],
+    "GRAY":["GRAY","GR","GY","GRY"]
+}
+
+for line in sys_stdin:
     vehiclecolor = line.split(",")[33].strip()
     vehiclecolor = vehiclecolor.strip("Vehicle Color")
 
     if vehiclecolor:
-        issuecolor = str(vehiclecolor)
+        testcolor = str(vehiclecolor).upper()
+        issuecolor = testcolor
+        for k,v in color_dict.items():
+            if testcolor in v:
+                issuecolor = k
         print("%s\t%s" % (issuecolor, 1))
